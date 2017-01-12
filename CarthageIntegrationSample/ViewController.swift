c4089c548516747a729801db359794d46aec32aa
Milen Halachev <milenski88@gmail.com>
2017-01-12 15:23:57 +0200

    Initial Commit

---
+++ CarthageIntegrationSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CarthageIntegrationSample
+//
+//  Created by Milen Halachev on 1/12/17.
+//  Copyright © 2017 Milen Halachev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
